b79d59282672f73e1063d84f2562211866a7b5fb
Tea-Sea <Tea-Sea@Github.com>
2025-04-02 10:58:20 +0100

    initial commit

---
+++ src/window.cpp
@@ -0,0 +1,13 @@
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <string>
+// #include <glad/glad.h>
+
+/* Constants */
+//Screen dimension constants
+constexpr int kScreenWidth{ 640 };
+constexpr int kScreenHeight{ 480 };
+
+int window () {
+    return 0;
+}
